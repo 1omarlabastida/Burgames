6a6aa27c4aeb2348b269c870e757811a53b0313f
omarlabastida <jlabastida@weecompany.net>
2022-07-20 00:20:05 -0500

    update location MainActivity

--- app/src/main/java/com/enydea/burgergames/ui/view/MainActivity.kt
+++ app/src/main/java/com/enydea/burgergames/ui/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.enydea.burgergames
+package com.enydea.burgergames.ui.view
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.enydea.burgergames.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
